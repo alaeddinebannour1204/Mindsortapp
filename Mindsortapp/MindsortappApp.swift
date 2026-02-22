615833c2e62b504a442ec425c7db0f0492e29571
alaeddinebannour1204 <alabannour@gmail.com>
2026-02-22 17:54:07 +0100

    Initial Commit

---
+++ Mindsortapp/MindsortappApp.swift
@@ -0,0 +1,32 @@
+//
+//  MindsortappApp.swift
+//  Mindsortapp
+//
+//  Created by Ala Eddine Bannour on 22.02.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MindsortappApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
